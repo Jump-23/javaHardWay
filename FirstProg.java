b862d11de8a2ad5c89497dcd17214628fb7241ce
Matthew <matthewejump@gmail.com>
2016-11-16 21:45:32 -0700

    Start over

--- FirstProg.java
+++
@@ -1,6 +0,0 @@
-public class FirstProg {
-	public static void main( String[] args ) {
-		System.out.println( "Matthew is determined to learn how to code." );
-		System.out.println( "Today's date is 11/14/16" );
-	}
-}
